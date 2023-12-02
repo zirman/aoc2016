a10f19bd0d45ff0dd8489c7fce21c8a70b0cb9bb
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-02 17:04:03 -0500

    Day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,20 +1,75 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        TODO()
+        val regex = """([a-z\-]+)-(\d+)\[([a-z]+)]""".toRegex()
+        input
+            .mapNotNull { line ->
+                val (a, b, c) = regex.matchEntire(line)!!.destructured
+
+                a
+                    .split('-')
+                    .flatMap { it.toList() }
+                    .groupingBy { it }
+                    .eachCount()
+                    .toList()
+                    .sortedWith(
+                        compareByDescending<Pair<Char, Int>> { (_, count) -> count }
+                            .then(compareBy { (c) -> c })
+                    )
+                    .take(5)
+                    .map { (c) -> c }
+                    .joinToString("") { it.toString() }
+                    .let { if (it == c) b.toInt() else null }
+            }
+            .sum()
+            .run { return this }
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val regex = """([a-z\-]+)-(\d+)\[([a-z]+)]""".toRegex()
+        input
+            .mapNotNull { line ->
+                val (a, b, c) = regex.matchEntire(line)!!.destructured
+
+                a
+                    .split('-')
+                    .flatMap { it.toList() }
+                    .groupingBy { it }
+                    .eachCount()
+                    .toList()
+                    .sortedWith(
+                        compareByDescending<Pair<Char, Int>> { (_, count) -> count }
+                            .then(compareBy { (c) -> c })
+                    )
+                    .take(5)
+                    .map { (c) -> c }
+                    .joinToString("") { it.toString() }
+                    .let { if (it == c) Pair(b.toInt(), a) else null }
+            }
+            .map { (shift, name) ->
+                name
+                    .map { c ->
+                        if (c.isLetter()) {
+                            run { c.code - 'a'.code }
+                                .let { it + shift }
+                                .let { it % 26 }
+                                .let { it + 'a'.code }
+                                .toChar()
+                        } else {
+                            ' '
+                        }
+                    }
+                    .joinToString("") { it.toString() }
+                    .let { Pair(it, shift) }
+            }
+            .first { (name) -> name == "northpole object storage" }
+            .let { (_, code) -> code }
+            .run { return this }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput1 = readInput("Day04_1_test")
-    check(part1(testInput1) == TODO())
-
-//    val testInput2 = readInput("Day04_2_test")
-//    check(part2(testInput2) == 1)
+    check(part1(testInput1) == 1514)
 
     val input = readInput("Day04")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
