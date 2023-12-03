6d198dd27fd325859d5ccce443ab3f292fcfbdc4
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-03 17:07:45 -0500

    Day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,20 +1,44 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        TODO()
+    fun part1(input: List<String>): String {
+        input[0]
+            .indices.map { i ->
+                input
+                    .map { line -> line[i] }
+                    .groupingBy { it }
+            }
+            .map { groupings ->
+                groupings
+                    .eachCount()
+                    .maxBy { (_, v) -> v }
+                    .key
+            }
+            .joinToString("") { it.toString() }
+            .run { return this }
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part2(input: List<String>): String {
+        input[0]
+            .indices.map { i ->
+                input
+                    .map { line -> line[i] }
+                    .groupingBy { it }
+            }
+            .map { groupings ->
+                groupings
+                    .eachCount()
+                    .minBy { (_, v) -> v }
+                    .key
+            }
+            .joinToString("") { it.toString() }
+            .run { return this }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput1 = readInput("Day06_1_test")
-    check(part1(testInput1) == TODO())
-
-//    val testInput2 = readInput("Day06_2_test")
-//    check(part2(testInput2) == 1)
+    check(part1(testInput1) == "easter")
+    check(part2(testInput1) == "advent")
 
     val input = readInput("Day06")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
