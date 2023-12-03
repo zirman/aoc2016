82bd38da7113324f71cda755f057547e5da3f14a
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-03 15:53:45 -0500

    Day 5

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,20 +1,44 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        TODO()
+    fun part1(input: String): String {
+        run { 0L..Long.MAX_VALUE }
+            .asSequence()
+            .mapNotNull { code ->
+                run { input + code }
+                    .md5()
+                    .takeIf { it.startsWith("00000") }
+                    ?.let { it[5] }
+            }
+            .take(8)
+            .joinToString("") { it.toString() }
+            .run { return this }
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part2(input: String): String {
+        run { 0L..Long.MAX_VALUE }
+            .asSequence()
+            .scan(listOf<Char?>(null, null, null, null, null, null, null, null)) { password, code ->
+                run { input + code }
+                    .md5()
+                    .takeIf { it.startsWith("00000") }
+                    ?.let { hash ->
+                        hash[5]
+                            .digitToIntOrNull()
+                            ?.takeIf { it in 0..7 }
+                            ?.takeIf { password[it] == null }
+                            ?.let { password.mapIndexed { index, c -> if (index == it) hash[6] else c } }
+                    }
+                    ?: password
+            }
+            .first { password -> password.all { it != null } }
+            .joinToString("") { it.toString() }
+            .run { return this }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput1 = readInput("Day05_1_test")
-    check(part1(testInput1) == TODO())
+    val testInput = "abc"
+    check(part1(testInput) == "18f47a30")
+    check(part2(testInput) == "05ace8e3")
 
-//    val testInput2 = readInput("Day05_2_test")
-//    check(part2(testInput2) == 1)
-
-    val input = readInput("Day05")
+    val input = "ojvtpuvg"
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
