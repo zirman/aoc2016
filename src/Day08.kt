a900d5bed9650fcbca2e29c412405f6a704b2047
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-04 21:16:47 -0500

    Day 8 & 9

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,20 +1,65 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        TODO()
+        val rectRegex = """rect (\d+)x(\d+)""".toRegex()
+        val rotateRowRegex = """rotate row y=(\d+) by (\d+)""".toRegex()
+        val rotateColumnRegex = """rotate column x=(\d+) by (\d+)""".toRegex()
+        val pixels = (1..6).map { (1..50).map { false }.toMutableList() }
+
+        input.forEach { line ->
+            when {
+                line.startsWith("rect") -> {
+                    val (a, b) = rectRegex.matchEntire(line)!!.destructured
+                    for (rowIndex in 0..<b.toInt()) {
+                        for (columnIndex in 0..<a.toInt()) {
+                            pixels[rowIndex][columnIndex] = true
+                        }
+                    }
+                }
+
+                line.startsWith("rotate row") -> {
+                    val (rowIndexString, byString) = rotateRowRegex.matchEntire(line)!!.destructured
+                    val rowIndex = rowIndexString.toInt()
+                    val row = pixels[rowIndex]
+                    val shift = (-byString.toInt()).mod(50)
+
+                    row.indices.map { columnIndex -> row[(columnIndex + shift).mod(50)] }
+                        .forEachIndexed { columnIndex, pixel ->
+                            row[columnIndex] = pixel
+                        }
+                }
+
+                line.startsWith("rotate column") -> {
+                    val (columnIndexString, byString) = rotateColumnRegex.matchEntire(line)!!.destructured
+                    val columnIndex = columnIndexString.toInt()
+                    val shift = (-byString.toInt()).mod(6)
+
+                    pixels.indices.map { rowIndex -> pixels[(rowIndex + shift).mod(6)][columnIndex] }
+                        .forEachIndexed { rowIndex, pixel ->
+                            pixels[rowIndex][columnIndex] = pixel
+                        }
+                }
+            }
+        }
+
+        pixels
+            .joinToString("\n") { line ->
+                line.joinToString("") { if (it) "#" else "." }
+            }
+            .also { println(it) }
+
+        return pixels.sumOf { row -> row.count { it } }
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput1 = readInput("Day08_1_test")
-    check(part1(testInput1) == TODO())
-
-//    val testInput2 = readInput("Day08_2_test")
-//    check(part2(testInput2) == 1)
+    check(
+        part1(
+            listOf(
+                "rect 3x2",
+                "rotate column x=1 by 1",
+                "rect 3x2",
+            )
+        ) == 7
+    )
 
     val input = readInput("Day08")
     part1(input).println()
-//    part2(input).println()
 }
--- src/Day09.kt
+++ src/Day09.kt
@@ -1,20 +1,55 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        TODO()
+    fun part1(input: String): Int {
+        val regex = """([A-Z]*)\((\d+)x(\d+)\)""".toRegex()
+        var i = 0
+        var s = input
+        while (true) {
+            s = buildString {
+                val match = regex.matchAt(s, i) ?: return s.length
+                val (text, lengthStr, repeatStr) = match.destructured
+                append(s.substring(0, i))
+                append(text)
+                val data = s.substring(match.range.last + 1, match.range.last + 1 + lengthStr.toInt())
+                (1..repeatStr.toInt())
+                    .forEach { append(data) }
+                i += text.length + (data.length * repeatStr.toInt())
+                append(s.substring(match.range.last + 1 + lengthStr.toInt(), s.length))
+            }
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part2(input: String): Long {
+        val repeatRegex = """([A-Z]*)\((\d+)x(\d+)\)([A-Z\dx()]+)""".toRegex()
+
+        fun recur(s: String): Long {
+            val match = repeatRegex.matchEntire(s)
+
+            return if (match != null) {
+                val (prefix, subLength, repeatCount, rest) = match.destructured
+                val subString = rest.substring(0, subLength.toInt())
+                val rest2 = rest.substring(subLength.toInt())
+                prefix.length + (repeatCount.toLong() * recur(subString)) + recur(rest2)
+            } else {
+                s.length.toLong()
+            }
+        }
+
+        return recur(input)
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput1 = readInput("Day09_1_test")
-    check(part1(testInput1) == TODO())
+    check(part1("ADVENT") == 6)
+    check(part1("A(1x5)BC") == 7)
+    check(part1("(3x3)XYZ") == 9)
+    check(part1("A(2x2)BCD(2x2)EFG") == 11)
+    check(part1("(6x1)(1x3)A") == 6)
+    check(part1("X(8x2)(3x3)ABCY") == 18)
 
-//    val testInput2 = readInput("Day09_2_test")
-//    check(part2(testInput2) == 1)
+    check(part2("(3x3)XYZ") == 9L)
+    check(part2("X(8x2)(3x3)ABCY") == 20L)
+    check(part2("(27x12)(20x12)(13x14)(7x10)(1x12)A") == 241920L)
+    check(part2("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN") == 445L)
 
     val input = readInput("Day09")
-    part1(input).println()
-//    part2(input).println()
+    part1(input[0]).println()
+    part2(input[0]).println()
 }
