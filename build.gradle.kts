7f8498040fcdaebb70a282997f568d7d93da9dc8
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-09 22:45:13 -0500

    Day 10

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     kotlin("jvm") version "1.9.20"
 }
 
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC")
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.6")
+}
+
 sourceSets {
     main {
         kotlin.srcDir("src")
--- src/Day10.kt
+++ src/Day10.kt
@@ -1,20 +1,137 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        TODO()
+    fun part1(input: List<String>): String {
+        val valueRegex = """value (\d+) goes to bot (\d+)""".toRegex()
+        val botRegex = """bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output) (\d+)""".toRegex()
+        val (initialValueLines, botsInstructionLines) = input.partition { valueRegex.matches(it) }
+
+        val bots = initialValueLines
+            .map { line -> valueRegex.matchEntire(line)!!.destructured }
+            .groupingBy { (_, botId) -> botId }
+            .aggregate { _, accumulator: MutableList<Int>?, (element), _ ->
+                (accumulator ?: mutableListOf()).apply { add(element.toInt()) }
+            }
+            .toMutableMap()
+
+        val insts = botsInstructionLines.associate { line ->
+            val (botId, low, lowId, high, highId) = botRegex.matchEntire(line)!!.destructured
+            Pair(botId, listOf(low, lowId, high, highId))
+        }
+
+        val outputs = mutableMapOf<String, MutableList<Int>>()
+        println(insts)
+
+        var didStuff = true
+        while (didStuff) {
+            didStuff = false
+            insts.forEach { (botId, inst) ->
+                if ((bots[botId]?.size ?: 0) > 2) throw IllegalStateException()
+
+                if (bots[botId]?.size == 2) {
+                    val (lowType, lowId, highType, highId) = inst
+
+                    if (
+                        (lowType != "bot" || bots[lowId].orEmpty().size < 2) &&
+                        (highType != "bot" || bots[highId].orEmpty().size < 2)
+                    ) {
+                        didStuff = true
+                        val low = bots[botId]!!.min()
+                        val high = bots[botId]!!.max()
+                        bots[botId]!!.clear()
+
+                        if (low == 17 && high == 61) {
+                            return botId
+                        }
+
+                        when (lowType) {
+                            "output" -> {
+                                outputs.getOrPut(lowId) { mutableListOf() }.add(low)
+                            }
+
+                            "bot" -> {
+                                bots.getOrPut(lowId) { mutableListOf() }.add(low)
+                            }
+                        }
+                        when (highType) {
+                            "output" -> {
+                                outputs.getOrPut(highId) { mutableListOf() }.add(high)
+                            }
+
+                            "bot" -> {
+                                bots.getOrPut(highId) { mutableListOf() }.add(high)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        throw IllegalStateException()
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val valueRegex = """value (\d+) goes to bot (\d+)""".toRegex()
+        val botRegex = """bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output) (\d+)""".toRegex()
+        val (initialValueLines, botsInstructionLines) = input.partition { valueRegex.matches(it) }
+
+        val bots = initialValueLines
+            .map { line -> valueRegex.matchEntire(line)!!.destructured }
+            .groupingBy { (_, botId) -> botId }
+            .aggregate { _, accumulator: MutableList<Int>?, (element), _ ->
+                (accumulator ?: mutableListOf()).apply { add(element.toInt()) }
+            }
+            .toMutableMap()
+
+        val insts = botsInstructionLines.associate { line ->
+            val (botId, low, lowId, high, highId) = botRegex.matchEntire(line)!!.destructured
+            Pair(botId, listOf(low, lowId, high, highId))
+        }
+
+        val outputs = mutableMapOf<String, MutableList<Int>>()
+        println(insts)
+
+        var didStuff = true
+        while (didStuff) {
+            didStuff = false
+            insts.forEach { (botId, inst) ->
+                if ((bots[botId]?.size ?: 0) > 2) throw IllegalStateException()
+
+                if (bots[botId]?.size == 2) {
+                    val (lowType, lowId, highType, highId) = inst
+
+                    if (
+                        (lowType != "bot" || bots[lowId].orEmpty().size < 2) &&
+                        (highType != "bot" || bots[highId].orEmpty().size < 2)
+                    ) {
+                        didStuff = true
+                        val low = bots[botId]!!.min()
+                        val high = bots[botId]!!.max()
+                        bots[botId]!!.clear()
+
+                        when (lowType) {
+                            "output" -> {
+                                outputs.getOrPut(lowId) { mutableListOf() }.add(low)
+                            }
+
+                            "bot" -> {
+                                bots.getOrPut(lowId) { mutableListOf() }.add(low)
+                            }
+                        }
+                        when (highType) {
+                            "output" -> {
+                                outputs.getOrPut(highId) { mutableListOf() }.add(high)
+                            }
+
+                            "bot" -> {
+                                bots.getOrPut(highId) { mutableListOf() }.add(high)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return outputs["0"]!![0] * outputs["1"]!![0] * outputs["2"]!![0]
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput1 = readInput("Day10_1_test")
-    check(part1(testInput1) == TODO())
-
-//    val testInput2 = readInput("Day10_2_test")
-//    check(part2(testInput2) == 1)
 
     val input = readInput("Day10")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
