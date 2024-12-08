d536aa6f9e2a7eb56d01b4184419f63e9f1bf511
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-12-07 19:06:39 -0500

    Day 10

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,10 +1,12 @@
+import kotlin.system.measureTimeMillis
+
 fun main() {
     fun part1(input: List<String>): String {
         val valueRegex = """value (\d+) goes to bot (\d+)""".toRegex()
         val botRegex = """bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output) (\d+)""".toRegex()
         val (initialValueLines, botsInstructionLines) = input.partition { valueRegex.matches(it) }
 
-        val bots = initialValueLines
+        val botValues = initialValueLines
             .map { line -> valueRegex.matchEntire(line)!!.destructured }
             .groupingBy { (_, botId) -> botId }
             .aggregate { _, accumulator: MutableList<Int>?, (element), _ ->
@@ -12,31 +14,28 @@ fun main() {
             }
             .toMutableMap()
 
-        val insts = botsInstructionLines.associate { line ->
+        val botInstructions = botsInstructionLines.associate { line ->
             val (botId, low, lowId, high, highId) = botRegex.matchEntire(line)!!.destructured
             Pair(botId, listOf(low, lowId, high, highId))
         }
 
-        val outputs = mutableMapOf<String, MutableList<Int>>()
-        println(insts)
+        val outputValues = mutableMapOf<String, MutableList<Int>>()
 
-        var didStuff = true
-        while (didStuff) {
-            didStuff = false
-            insts.forEach { (botId, inst) ->
-                if ((bots[botId]?.size ?: 0) > 2) throw IllegalStateException()
-
-                if (bots[botId]?.size == 2) {
+        var didSomething: Boolean
+        do {
+            didSomething = false
+            botInstructions.forEach { (botId, inst) ->
+                if (botValues[botId]?.size == 2) {
                     val (lowType, lowId, highType, highId) = inst
 
                     if (
-                        (lowType != "bot" || bots[lowId].orEmpty().size < 2) &&
-                        (highType != "bot" || bots[highId].orEmpty().size < 2)
+                        (lowType != "bot" || botValues[lowId].orEmpty().size < 2) &&
+                        (highType != "bot" || botValues[highId].orEmpty().size < 2)
                     ) {
-                        didStuff = true
-                        val low = bots[botId]!!.min()
-                        val high = bots[botId]!!.max()
-                        bots[botId]!!.clear()
+                        didSomething = true
+                        val low = botValues[botId]!!.min()
+                        val high = botValues[botId]!!.max()
+                        botValues[botId]!!.clear()
 
                         if (low == 17 && high == 61) {
                             return botId
@@ -44,26 +43,26 @@ fun main() {
 
                         when (lowType) {
                             "output" -> {
-                                outputs.getOrPut(lowId) { mutableListOf() }.add(low)
+                                outputValues.getOrPut(lowId) { mutableListOf() }.add(low)
                             }
 
                             "bot" -> {
-                                bots.getOrPut(lowId) { mutableListOf() }.add(low)
+                                botValues.getOrPut(lowId) { mutableListOf() }.add(low)
                             }
                         }
                         when (highType) {
                             "output" -> {
-                                outputs.getOrPut(highId) { mutableListOf() }.add(high)
+                                outputValues.getOrPut(highId) { mutableListOf() }.add(high)
                             }
 
                             "bot" -> {
-                                bots.getOrPut(highId) { mutableListOf() }.add(high)
+                                botValues.getOrPut(highId) { mutableListOf() }.add(high)
                             }
                         }
                     }
                 }
             }
-        }
+        } while (didSomething)
         throw IllegalStateException()
     }
 
@@ -86,7 +85,6 @@ fun main() {
         }
 
         val outputs = mutableMapOf<String, MutableList<Int>>()
-        println(insts)
 
         var didStuff = true
         while (didStuff) {
@@ -132,6 +130,6 @@ fun main() {
     }
 
     val input = readInput("Day10")
-    part1(input).println()
-    part2(input).println()
+    measureTimeMillis { part1(input).println() }.also { println("time: $it") }
+    measureTimeMillis { part2(input).println() }.also { println("time: $it") }
 }
--- src/Day11.kt
+++ src/Day11.kt
@@ -1,6 +1,201 @@
+import kotlinx.collections.immutable.PersistentSet
+import kotlinx.collections.immutable.persistentSetOf
+import kotlin.math.max
+import kotlin.math.min
+
+sealed interface RadioisotopeItem {
+    val string: String
+
+    @JvmInline
+    value class Microchip(override val string: String) : RadioisotopeItem
+
+    @JvmInline
+    value class Generator(override val string: String) : RadioisotopeItem
+}
+
+typealias Floors = List<Set<RadioisotopeItem>>
+
+data class ElevatorState(
+    val elevator: Int,
+    val floors: Floors,
+)
+
+fun <T> List<T>.permute(): List<Pair<T, T>> {
+    return flatMapIndexed { index: Int, t1: T ->
+        this@permute.subList(index, this@permute.size).map { t2 -> Pair(t1, t2) }
+    }
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        TODO()
+        val floorRegex = """The (\w+) floor contains (.*)\.""".toRegex()
+        val generatorRegex = """a (\w+) generator""".toRegex()
+        val microchipRegex = """a (\w+)-compatible microchip""".toRegex()
+        val initialFloors = input.map { line ->
+            val (_, itemsStr) = floorRegex.matchEntire(line)!!.destructured
+
+            buildSet {
+                if (itemsStr != "nothing relevant") {
+                    itemsStr
+                        .split(", ", ", and ", " and ")
+                        .forEach { itemStr ->
+                            microchipRegex.matchEntire(itemStr)
+                                ?.destructured
+                                ?.let { (microchipType) -> add(RadioisotopeItem.Microchip(microchipType)) }
+
+                            generatorRegex.matchEntire(itemStr)
+                                ?.destructured
+                                ?.let { (generatorType) -> add(RadioisotopeItem.Generator(generatorType)) }
+                        }
+                }
+            }
+        }
+
+        val sortedItems = initialFloors
+            .reduce { a, b -> a + b }
+            .sortedWith(compareBy<RadioisotopeItem> {
+                it.string
+            }.thenBy {
+                when (it) {
+                    is RadioisotopeItem.Microchip -> 1
+                    is RadioisotopeItem.Generator -> 0
+                }
+            })
+            .toSet()
+
+        fun ElevatorState.asString(): String {
+            floors
+                .mapIndexed { index, radioisotopeItems -> Pair(index, radioisotopeItems) }
+                .reversed()
+                .joinToString("\n") { (floor, items) ->
+                    "F${floor + 1} ${if (elevator == floor) 'E' else '.'}  ${
+                        sortedItems.joinToString(" ") {
+                            if (items.contains(it)) {
+                                "${it.string[0].uppercase()}${
+                                    when (it) {
+                                        is RadioisotopeItem.Generator -> "G"
+                                        is RadioisotopeItem.Microchip -> "M"
+                                    }
+                                }"
+                            } else {
+                                ". "
+                            }
+                        }
+                    } "
+                }
+                .run { return this }
+        }
+
+        val finalState = ElevatorState(
+            elevator = 3,
+            floors = listOf(
+                emptySet(),
+                emptySet(),
+                emptySet(),
+                sortedItems,
+            ),
+        )
+
+        finalState.asString().also { println(it) }
+
+        fun ElevatorState.isValid(): Boolean {
+            return floors
+                .any { items ->
+                    items.filterIsInstance<RadioisotopeItem.Microchip>()
+                        .any { microchip -> items.contains(RadioisotopeItem.Generator(microchip.string)).not() } &&
+                            items.any { it is RadioisotopeItem.Generator }
+                }
+                .not()
+        }
+
+        fun ElevatorState.nextStates(visited: PersistentSet<ElevatorState>): List<ElevatorState> {
+            val (currentFloor, items) = this
+
+            run { max(currentFloor - 1, 0)..min(currentFloor + 1, 3) }
+                .filter { it != currentFloor }
+                .flatMap { nextFloor ->
+
+                    items[currentFloor].toList().permute()
+                        .map { movedItems ->
+//                            println("$nextFloor ${movedItems}")
+                            val (item1, item2) = movedItems
+                            ElevatorState(
+                                nextFloor,
+                                items.mapIndexed { index, radioisotopeItems ->
+                                    when (index) {
+                                        currentFloor -> radioisotopeItems - listOf(item1, item2).toSet()
+                                        nextFloor -> radioisotopeItems + listOf(item1, item2)
+                                        else -> radioisotopeItems
+                                    }
+                                },
+                            )
+                        }
+                }
+//                .also {
+//                    it.forEach {
+//                        it.asString().also { println(it) }
+//                        kotlin.io.println()
+//                    }
+//                    println("wat")
+//                }
+                .filter { visited.contains(it).not() && it.isValid() }
+//                .also {
+//                    it.forEach {
+//                        it.asString().also { println(it) }
+//                        kotlin.io.println()
+//                    }
+//                    println("wut")
+//                }
+                .run { return this }
+        }
+
+
+        tailrec fun Set<ElevatorState>.breathFirstSearch(depth: Int, visited: PersistentSet<ElevatorState>): Int {
+//            this.forEach {
+//                it.asString().also { println(it) }
+//                kotlin.io.println()
+//            }
+//            if (depth >= 3) return depth
+
+//            if (this.contains(finalState)) return depth
+            return run {
+                flatMap {
+//                    it.asString().also { println(it) }
+//                    println("from $depth")
+                    it.nextStates(visited)
+//                        .also {
+//                            it.forEach {
+//                                it.asString().also { println(it) }
+//                                kotlin.io.println()
+//                            }
+//                            println("wat")
+//                        }
+                }
+//                    .filter { visited.contains(it).not() }
+                    .toSet()
+                    .takeIf { it.contains(finalState).not() }
+                    ?: return depth
+            }.breathFirstSearch(
+                depth = depth + 1,
+                visited = visited.addAll(this),
+            )
+        }
+//        listOf(
+//            emptySet(),
+//            emptySet(),
+//            setOf(
+//                RadioisotopeItem.Microchip("hydrogen"),
+//                RadioisotopeItem.Microchip("lithium"),
+//            ),
+//            setOf(
+//                RadioisotopeItem.Generator("hydrogen"),
+//                RadioisotopeItem.Generator("lithium"),
+//            ),
+//        )
+//        )
+
+        return setOf(ElevatorState(elevator = 0, floors = initialFloors))
+            .breathFirstSearch(depth = 0, visited = persistentSetOf()) + 1
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +204,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput1 = readInput("Day11_1_test")
-    check(part1(testInput1) == TODO())
+    check(part1(testInput1).also { println(it) } == 11)
 
 //    val testInput2 = readInput("Day11_2_test")
 //    check(part2(testInput2) == 1)
