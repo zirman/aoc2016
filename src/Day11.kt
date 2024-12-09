a6e116de6860a6b6ab4a555d838d25816c452339
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-12-08 22:41:05 -0500

    Day 11

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,215 +1,208 @@
-import kotlinx.collections.immutable.PersistentSet
-import kotlinx.collections.immutable.persistentSetOf
-import kotlin.math.max
-import kotlin.math.min
+private data class RTG(val generatorFloor: Int, val microchipFloor: Int)
 
-sealed interface RadioisotopeItem {
-    val string: String
-
-    @JvmInline
-    value class Microchip(override val string: String) : RadioisotopeItem
-
-    @JvmInline
-    value class Generator(override val string: String) : RadioisotopeItem
-}
-
-typealias Floors = List<Set<RadioisotopeItem>>
-
-data class ElevatorState(
+private data class RTGState(
+    val rtgs: List<RTG>, // keep sorted to reduce search space
     val elevator: Int,
-    val floors: Floors,
 )
 
-fun <T> List<T>.permute(): List<Pair<T, T>> {
-    return flatMapIndexed { index: Int, t1: T ->
-        this@permute.subList(index, this@permute.size).map { t2 -> Pair(t1, t2) }
+private val rtgComparator = compareBy<RTG> { t -> t.generatorFloor }.thenBy { it.microchipFloor }
+
+private fun RTGState.isValid(): Boolean {
+//    if (elevator !in 1..4) {
+//        return false
+//    }
+    for (i in rtgs.indices) {
+//        if (rtgs[i].microchipFloor !in 1..4 ||
+//            rtgs[i].generatorFloor !in 1..4
+//        ) {
+//            return false
+//        }
+        if (rtgs[i].microchipFloor != rtgs[i].generatorFloor) {
+            for (k in rtgs.indices) {
+                if (i != k && rtgs[i].microchipFloor == rtgs[k].generatorFloor) {
+                    return false
+                }
+            }
+        }
     }
+    return true
+}
+
+private fun RTGState.isFinished(): Boolean {
+    return elevator == 4 && rtgs.all { rtg -> rtg.generatorFloor == 4 && rtg.microchipFloor == 4 }
+}
+
+private fun RTGState.nextStates(): Set<RTGState> {
+    val movableRtgs = rtgs.flatMapIndexed { i, rtg ->
+        buildList {
+            if (rtg.generatorFloor == elevator) {
+                add(Pair<Int, RTG.(Int) -> RTG>(i) { copy(generatorFloor = it) })
+            }
+            if (rtg.microchipFloor == elevator) {
+                add(Pair<Int, RTG.(Int) -> RTG>(i) { copy(microchipFloor = it) })
+            }
+        }
+    }
+    return (movableRtgs.combinations(1) + movableRtgs.combinations(2))
+        .flatMap { movingRtgs ->
+            buildList {
+                var newFloor = elevator + 1
+                if (newFloor <= 4) {
+                    add(
+                        RTGState(
+                            rtgs = movingRtgs.fold(rtgs) { rtgs, (isotope, update) ->
+                                rtgs.mapIndexed { i, rtg ->
+                                    if (isotope == i) {
+                                        rtg.update(newFloor)
+                                    } else {
+                                        rtg
+                                    }
+                                }
+                            }.sortedWith(rtgComparator),
+                            elevator = newFloor,
+                        ),
+                    )
+                }
+                newFloor = elevator - 1
+                if (newFloor >= 1) {
+                    add(
+                        RTGState(
+                            rtgs = movingRtgs.fold(rtgs) { rtgs, (isotope, update) ->
+                                rtgs.mapIndexed { i, rtg ->
+                                    if (isotope == i) {
+                                        rtg.update(newFloor)
+                                    } else {
+                                        rtg
+                                    }
+                                }
+                            }.sortedWith(rtgComparator),
+                            elevator = newFloor,
+                        ),
+                    )
+                }
+            }
+        }
+        .toSet()
 }
 
 fun main() {
     fun part1(input: List<String>): Int {
-        val floorRegex = """The (\w+) floor contains (.*)\.""".toRegex()
-        val generatorRegex = """a (\w+) generator""".toRegex()
-        val microchipRegex = """a (\w+)-compatible microchip""".toRegex()
-        val initialFloors = input.map { line ->
-            val (_, itemsStr) = floorRegex.matchEntire(line)!!.destructured
-
-            buildSet {
+        val floorRegex = """The [a-z]+ floor contains (.*)\.""".toRegex(RegexOption.IGNORE_CASE)
+        val generatorRegex = """([a-z]+) generator""".toRegex(RegexOption.IGNORE_CASE)
+        val microchipRegex = """([a-z]+)-compatible microchip""".toRegex(RegexOption.IGNORE_CASE)
+        val (generators, microchips) = input
+            .foldIndexed(Pair(emptyMap<String, Int>(), emptyMap<String, Int>())) { floor, (a, b), line ->
+                val (itemsStr) = floorRegex.matchEntire(line)!!.destructured
                 if (itemsStr != "nothing relevant") {
-                    itemsStr
-                        .split(", ", ", and ", " and ")
-                        .forEach { itemStr ->
-                            microchipRegex.matchEntire(itemStr)
-                                ?.destructured
-                                ?.let { (microchipType) -> add(RadioisotopeItem.Microchip(microchipType)) }
-
-                            generatorRegex.matchEntire(itemStr)
-                                ?.destructured
-                                ?.let { (generatorType) -> add(RadioisotopeItem.Generator(generatorType)) }
-                        }
+                    var generators = a
+                    var microchips = b
+                    var match = microchipRegex.find(itemsStr)
+                    while (match != null) {
+                        val (microchipType) = match.destructured
+                        microchips = microchips.plus(microchipType to floor + 1)
+                        match = match.next()
+                    }
+                    match = generatorRegex.find(itemsStr)
+                    while (match != null) {
+                        val (generatorType) = match.destructured
+                        generators = generators.plus(generatorType to floor + 1)
+                        match = match.next()
+                    }
+                    Pair(generators, microchips)
+                } else {
+                    Pair(a, b)
                 }
             }
-        }
-
-        val sortedItems = initialFloors
-            .reduce { a, b -> a + b }
-            .sortedWith(compareBy<RadioisotopeItem> {
-                it.string
-            }.thenBy {
-                when (it) {
-                    is RadioisotopeItem.Microchip -> 1
-                    is RadioisotopeItem.Generator -> 0
+        val initialState = RTGState(
+            rtgs = generators
+                .map { (name, generatorFloor) ->
+                    RTG(
+                        generatorFloor = generatorFloor,
+                        microchipFloor = microchips[name]!!
+                    )
                 }
-            })
-            .toSet()
-
-        fun ElevatorState.asString(): String {
-            floors
-                .mapIndexed { index, radioisotopeItems -> Pair(index, radioisotopeItems) }
-                .reversed()
-                .joinToString("\n") { (floor, items) ->
-                    "F${floor + 1} ${if (elevator == floor) 'E' else '.'}  ${
-                        sortedItems.joinToString(" ") {
-                            if (items.contains(it)) {
-                                "${it.string[0].uppercase()}${
-                                    when (it) {
-                                        is RadioisotopeItem.Generator -> "G"
-                                        is RadioisotopeItem.Microchip -> "M"
-                                    }
-                                }"
-                            } else {
-                                ". "
-                            }
-                        }
-                    } "
-                }
-                .run { return this }
-        }
-
-        val finalState = ElevatorState(
-            elevator = 3,
-            floors = listOf(
-                emptySet(),
-                emptySet(),
-                emptySet(),
-                sortedItems,
-            ),
+                .sortedWith(rtgComparator),
+            elevator = 1,
         )
-
-        finalState.asString().also { println(it) }
-
-        fun ElevatorState.isValid(): Boolean {
-            return floors
-                .any { items ->
-                    items.filterIsInstance<RadioisotopeItem.Microchip>()
-                        .any { microchip -> items.contains(RadioisotopeItem.Generator(microchip.string)).not() } &&
-                            items.any { it is RadioisotopeItem.Generator }
-                }
-                .not()
+        val visited = mutableSetOf<RTGState>()
+        var states = listOf(initialState)
+        var moves = 0
+        while (true) {
+            if (states.any { it.isFinished() }) {
+                return moves
+            }
+            states = states.flatMap { state ->
+                state.nextStates()
+                    .filter { it !in visited && it.isValid() }
+                    .also {
+                        visited += it
+                    }
+            }
+            moves++
         }
-
-        fun ElevatorState.nextStates(visited: PersistentSet<ElevatorState>): List<ElevatorState> {
-            val (currentFloor, items) = this
-
-            run { max(currentFloor - 1, 0)..min(currentFloor + 1, 3) }
-                .filter { it != currentFloor }
-                .flatMap { nextFloor ->
-
-                    items[currentFloor].toList().permute()
-                        .map { movedItems ->
-//                            println("$nextFloor ${movedItems}")
-                            val (item1, item2) = movedItems
-                            ElevatorState(
-                                nextFloor,
-                                items.mapIndexed { index, radioisotopeItems ->
-                                    when (index) {
-                                        currentFloor -> radioisotopeItems - listOf(item1, item2).toSet()
-                                        nextFloor -> radioisotopeItems + listOf(item1, item2)
-                                        else -> radioisotopeItems
-                                    }
-                                },
-                            )
-                        }
-                }
-//                .also {
-//                    it.forEach {
-//                        it.asString().also { println(it) }
-//                        kotlin.io.println()
-//                    }
-//                    println("wat")
-//                }
-                .filter { visited.contains(it).not() && it.isValid() }
-//                .also {
-//                    it.forEach {
-//                        it.asString().also { println(it) }
-//                        kotlin.io.println()
-//                    }
-//                    println("wut")
-//                }
-                .run { return this }
-        }
-
-
-        tailrec fun Set<ElevatorState>.breathFirstSearch(depth: Int, visited: PersistentSet<ElevatorState>): Int {
-//            this.forEach {
-//                it.asString().also { println(it) }
-//                kotlin.io.println()
-//            }
-//            if (depth >= 3) return depth
-
-//            if (this.contains(finalState)) return depth
-            return run {
-                flatMap {
-//                    it.asString().also { println(it) }
-//                    println("from $depth")
-                    it.nextStates(visited)
-//                        .also {
-//                            it.forEach {
-//                                it.asString().also { println(it) }
-//                                kotlin.io.println()
-//                            }
-//                            println("wat")
-//                        }
-                }
-//                    .filter { visited.contains(it).not() }
-                    .toSet()
-                    .takeIf { it.contains(finalState).not() }
-                    ?: return depth
-            }.breathFirstSearch(
-                depth = depth + 1,
-                visited = visited.addAll(this),
-            )
-        }
-//        listOf(
-//            emptySet(),
-//            emptySet(),
-//            setOf(
-//                RadioisotopeItem.Microchip("hydrogen"),
-//                RadioisotopeItem.Microchip("lithium"),
-//            ),
-//            setOf(
-//                RadioisotopeItem.Generator("hydrogen"),
-//                RadioisotopeItem.Generator("lithium"),
-//            ),
-//        )
-//        )
-
-        return setOf(ElevatorState(elevator = 0, floors = initialFloors))
-            .breathFirstSearch(depth = 0, visited = persistentSetOf()) + 1
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val floorRegex = """The [a-z]+ floor contains (.*)\.""".toRegex(RegexOption.IGNORE_CASE)
+        val generatorRegex = """([a-z]+) generator""".toRegex(RegexOption.IGNORE_CASE)
+        val microchipRegex = """([a-z]+)-compatible microchip""".toRegex(RegexOption.IGNORE_CASE)
+        val (generators, microchips) = input
+            .foldIndexed(Pair(emptyMap<String, Int>(), emptyMap<String, Int>())) { floor, (a, b), line ->
+                val (itemsStr) = floorRegex.matchEntire(line)!!.destructured
+                if (itemsStr != "nothing relevant") {
+                    var generators = a
+                    var microchips = b
+                    var match2 = microchipRegex.find(itemsStr)
+                    while (match2 != null) {
+                        val (microchipType) = match2.destructured
+                        microchips = microchips.plus(microchipType to floor + 1)
+                        match2 = match2.next()
+                    }
+                    match2 = generatorRegex.find(itemsStr)
+                    while (match2 != null) {
+                        val (generatorType) = match2.destructured
+                        generators = generators.plus(generatorType to floor + 1)
+                        match2 = match2.next()
+                    }
+                    Pair(generators, microchips)
+                } else {
+                    Pair(a, b)
+                }
+            }
+        val initialState = RTGState(
+            rtgs = generators
+                .map { (name, generatorFloor) ->
+                    RTG(
+                        generatorFloor = generatorFloor,
+                        microchipFloor = microchips[name]!!
+                    )
+                }
+                .plus(RTG(1, 1))
+                .plus(RTG(1, 1))
+                .sortedWith(rtgComparator),
+            elevator = 1,
+        )
+        val visited = mutableSetOf<RTGState>()
+        var states = listOf(initialState)
+        var moves = 0
+        while (true) {
+            if (states.any { it.isFinished() }) {
+                return moves
+            }
+            states = states.flatMap { state ->
+                state.nextStates()
+                    .filter { it !in visited && it.isValid() }
+                    .also {
+                        visited += it
+                    }
+            }
+            moves++
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput1 = readInput("Day11_1_test")
     check(part1(testInput1).also { println(it) } == 11)
-
-//    val testInput2 = readInput("Day11_2_test")
-//    check(part2(testInput2) == 1)
-
     val input = readInput("Day11")
-    part1(input).println()
-//    part2(input).println()
+    printlnMeasureTimeMillis { part1(input).println() }
+    printlnMeasureTimeMillis { part2(input).println() }
 }
