e01d87eba7ee6ca3609834383ae1b66bc99ed6f0
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-12-10 23:32:33 -0500

    Day 14

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,20 +1,54 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        TODO()
+    fun part1(input: String): Int {
+        return sequence {
+            for (i in 0..Int.MAX_VALUE) {
+                yield("$input$i".md5())
+            }
+        }
+            .windowed(1000)
+            .withIndex()
+            .filter { (_, hashes) ->
+                hashes.first()
+                    .windowed(3)
+                    .find { it.all { char -> char == it[0] } }
+                    ?.let { triple ->
+                        val pent = (1..5).joinToString("") { "${triple[0]}" }
+                        hashes.drop(1).any { it.contains(pent) }
+                    } == true
+            }
+            .take(64)
+            .last()
+            .index
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part2(input: String): Int {
+        return sequence {
+            for (i in 0..Int.MAX_VALUE) {
+                var x = "$input$i"
+                repeat(2017) {
+                    x = x.md5()
+                }
+                yield(x)
+            }
+        }
+            .windowed(1000)
+            .withIndex()
+            .filter { (_, hashes) ->
+                hashes.first()
+                    .windowed(3)
+                    .find { it.all { char -> char == it[0] } }
+                    ?.let { triple ->
+                        val pent = (1..5).joinToString("") { "${triple[0]}" }
+                        hashes.drop(1).any { it.contains(pent) }
+                    } == true
+            }
+            .take(64)
+            .last()
+            .index
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput1 = readInput("Day14_1_test")
-    check(part1(testInput1) == TODO())
-
-//    val testInput2 = readInput("Day14_2_test")
-//    check(part2(testInput2) == 1)
-
-    val input = readInput("Day!4")
-    part1(input).println()
+    check(part1("abc") == 22728)
+    printlnMeasureTimeMillis { part1("ngcjuoqr").println() }
+    check(part2("abc") == 22551)
+    printlnMeasureTimeMillis { part2("ngcjuoqr").println() }
 //    part2(input).println()
 }
