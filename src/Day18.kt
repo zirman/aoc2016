b52bc9f40ac40b90710218987753fdc531579b42
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-12-14 20:16:43 -0500

    Day 18

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,6 +1,33 @@
+typealias Input14 = String
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        TODO()
+    fun String.parse(): String {
+        return this
+    }
+
+    fun String.next(): String = ".$this.".windowed(3).joinToString("") { kernel ->
+        val left = kernel[0] == '^'
+        val center = kernel[1] == '^'
+        val right = kernel[2] == '^'
+        if ((left && center && !right) ||
+            (!left && center && right) ||
+            (left && !center && !right) ||
+            (!left && !center && right)
+        ) "^" else "."
+    }
+
+    fun String.part1(count: Int): Int {
+//        Its left and center tiles are traps, but its right tile is not.
+//        Its center and right tiles are traps, but its left tile is not.
+//        Only its left tile is a trap.
+//        Only its right tile is a trap.
+        var x = this
+        var c = count { it == '.' }
+        for (i in 1..<count) {
+            x = x.next()
+            c += x.count { it == '.' }
+        }
+        return c
     }
 
     fun part2(input: List<String>): Int {
@@ -8,13 +35,9 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput1 = readInput("Day18_1_test")
-    check(part1(testInput1) == TODO())
-
-//    val testInput2 = readInput("Day18_2_test")
-//    check(part2(testInput2) == 1)
-
-    val input = readInput("Day18")
-    part1(input).println()
-//    part2(input).println()
+    val testInput = ".^^.^.^^^^"
+    check(testInput.part1(10) == 38)
+    val input = "^.^^^.^..^....^^....^^^^.^^.^...^^.^.^^.^^.^^..^.^...^.^..^.^^.^..^.....^^^.^.^^^..^^...^^^...^...^."
+    printlnMeasureTimeMillis { input.part1(40).println() }
+    printlnMeasureTimeMillis { input.part1(400000).println() }
 }
